74eb2b44dbf3ce0cd5ce9ac431290633faf46af4
BrandonMarques <brandonmmiranda@gmail.com>
2020-01-20 09:50:15 +0200

    Update 14

---
+++ Brandon_Miranda_GADE5112_POE/Assets/Scripts/WizardUnit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+class WizardUnit : Unit
+{
+    public WizardUnit(int x, int y, string faction) : base(x, y, 100, 3, 10, 5, faction, '*', "Sorceress") { }
+
+    public WizardUnit(string values) : base(values) { }
+
+    //public override string Save()
+    //{
+    //    return string.Format(
+    //        $"Wizard,{x},{y},{health},{maxHealth},{speed},{attack},{attackRange}," +
+    //        $"{faction},{symbol},{name},{isDestroyed}"
+    //    );
+    //}
+}
+
